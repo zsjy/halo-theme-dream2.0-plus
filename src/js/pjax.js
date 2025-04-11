b4e2c0536a6b315561894a67933b85120e62b797
mjsoftware <1406993111@qq.com>
2025-04-11 17:56:53 +0800

    支持控制banner的高度，支持快速下滑到文章区域

--- src/js/pjax.js
+++ src/js/pjax.js
@@ -17,7 +17,7 @@ const computeScrollTop = (target) => {
   if (target.pathname !== '/' && $('.banner').length !== 0) {
     // 避免跳转时顶部导航栏收缩
     window.initTop = 99999999
-    return window.innerHeight / 4
+    return window.innerHeight / 2
   }
   return 0
 }
