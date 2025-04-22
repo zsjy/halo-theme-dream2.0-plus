f4f8d10ba1f7ccc6a89fc9e0889a55930478ce2e
mjsoftware <1406993111@qq.com>
2025-04-22 17:19:02 +0800

    新增 量子丝线特效

--- src/js/common.js
+++ src/js/common.js
@@ -669,6 +669,7 @@ const commonContext = {
     DreamConfig.effects_snowflake_mode && Utils.cachedScript(`${DreamConfig.theme_base}/js/effects/snowflake.min.js?mew=${DreamConfig.theme_version}`)
     DreamConfig.effects_universe_mode && Utils.cachedScript(`${DreamConfig.theme_base}/js/effects/universe.min.js?mew=${DreamConfig.theme_version}`)
     DreamConfig.effects_circle_magic_mode && Utils.cachedScript(`${DreamConfig.theme_base}/js/effects/circleMagic.min.js?mew=${DreamConfig.theme_version}`)
+    DreamConfig.effects_quantum_silk_thread_mode && Utils.cachedScript(`${DreamConfig.theme_base}/js/effects/quantum.min.js?mew=${DreamConfig.theme_version}`)
   },
   /* 显示主题版本信息 */
   showThemeVersion() {
